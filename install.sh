234755f8067c441ef11300bc3e7b1bcc58836d4d
rbparrish <robparrish@Robs-MacBook-Air.local>
2013-10-17 09:44:38 -0700

    added new install instructions, removed virtualbox

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# meta updates
+
+
+sudo apt-get install python-pip
+sudo pip install requests
+#sudo pip install boto
+sudo apt-get update
+sudo apt-get install python-sklearn
+sudo apt-get install ipython python-dev gfortran libatlas-base-dev build-essential
+sudo pip install networkx pymc
+sudo pip install pandas
+sudo pip install beautifulsoup4
+sudo pip install nltk
+sudo pip install Flask
+
+sudo pip install -U scikit-learn
